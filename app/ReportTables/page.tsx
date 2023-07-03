b9ce72ee88be9a041f6c836e26140af086ee13e6
mungaben <kamaumungai3742@gmail.com>
2023-07-03 23:36:13 +0300

    RMS REPORT PAGE READY FOR DB

---
+++ app/ReportTables/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import MyTable from "./components/Table";
+
+
+const page = () => {
+  return (
+    <div className="  max-h-screen">
+     
+      <div className="ml-5"> 
+        <MyTable />
+      </div>
+    </div>
+  );
+};
+
+export default page;
