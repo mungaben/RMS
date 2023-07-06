6c853f9abac396f99516bc46c7eab5f61cd1b98f
mungaben <kamaumungai3742@gmail.com>
2023-07-06 22:21:29 +0300

    overflow scroll

--- app/DashBoard/components/Basis2/Basis2Data.tsx
+++ app/DashBoard/components/Basis2/Basis2Data.tsx
@@ -16,15 +16,15 @@ const Basis2Data = () => {
  
 
   return (
-    <div className=' flex flex-col bg-red-400 p-4'>
+    <div className=' flex flex-col bg-red-400 p-4 justify-center items-center'>
      
-      <div className=' w-full justify-center flex items-center'>
+      <div className=' w-full flex items-center justify-between ' >
         
         <div>
           <TopBar/>
         </div>
       </div>
-      <div className=' flex  md:mt-10 md:bottom-6 border-[1px] shadow-md bg-white'>
+      <div className=' flex  md:mt-10 md:bottom-6 border-[1px] shadow-md bg-white p-5 '>
       <LineChart  System={system} />
       </div>
      
