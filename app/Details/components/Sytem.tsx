9d721b0ed6f996e34165223baef426d76db74bff
mungaben <kamaumungai3742@gmail.com>
2023-07-07 09:27:08 +0300

    added mysterious

---
+++ app/Details/components/Sytem.tsx
@@ -0,0 +1,17 @@
+
+"use client"
+
+import React, { useEffect } from 'react'
+
+const Sytem = () => {
+
+    useEffect(() => {
+        console.log('Sytem')
+    }, [])
+
+  return (
+    <div>Sytem</div>
+  )
+}
+
+export default Sytem
