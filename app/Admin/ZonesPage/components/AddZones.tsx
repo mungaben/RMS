d13b24c422d8cfd0993e01d62c4ddb20ecf6f6ba
mungaben <kamaumungai3742@gmail.com>
2023-07-11 12:54:36 +0300

    ++create /del/put zones

---
+++ app/Admin/ZonesPage/components/AddZones.tsx
@@ -0,0 +1,15 @@
+
+
+
+
+"use client"
+
+import React from 'react'
+
+const AddZones = () => {
+  return (
+    <div>AddZones</div>
+  )
+}
+
+export default AddZones
---
+++ app/Admin/ZonesPage/components/DeleteZone.tsx
@@ -0,0 +1,14 @@
+
+
+
+"use client"
+
+import React from 'react'
+
+const DeleteZone = () => {
+  return (
+    <div>DeleteZone</div>
+  )
+}
+
+export default DeleteZone
