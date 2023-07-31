fe025e9eab616e109c2908b1b25f1b41034be02b
mungaben <kamaumungai3742@gmail.com>
2023-07-31 19:47:31 +0300

    style(sIgn style): :lipstick: ui :signIn sign up centerd divs
    ui:sign in sign up uis
    
    ui :sign up sign in

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -6,9 +6,12 @@ import { SignUp } from "@clerk/nextjs";
 const page = () => {
     // after sign up post data to db and check if user is admin if admin take to admin page else take to user page{"/"}
   return (
-    <div className='flex items-center justify-center '>
-         <SignUp afterSignUpUrl={'/'}/>
+    <div className='flex items-center justify-center w-full h-screen pt-10 group'>
+    <div className="flex items-center justify-center w-full p-4 mx-5 my-auto bg-transparent h-96 group-hover:opacity-80 group-hover:scale-110">
+    <SignUp afterSignInUrl={"/"} />
     </div>
+          
+  </div>
   )
 }
 
