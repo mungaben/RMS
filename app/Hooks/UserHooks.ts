d7a8ac3aafda74bee2d4480b1446bc3998209b55
mungaben <kamaumungai3742@gmail.com>
2023-08-15 10:56:56 +0300

    style:framer motion

---
+++ app/Hooks/UserHooks.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+import useSWR from "swr"
+
+
+
+
+// fetcher
+
+
+
+const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+
+
+const { isLoading, data, error,mutate } = useSWR("/api/Users", fetcher)
+
+
+export {
+    isLoading,
+    data,
+    error,
+    mutate
+}
